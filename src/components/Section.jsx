b22a4f3893d95b1934d2223a4fc621e181493138
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-03-09 20:16:35 +0100

    section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
 
-function Section() {
+function Section({className, children, id, crosses,crossesOffset, customPadding}) {
   return (
-    <div>Section</div>
+    <div className={`relative ${customPadding || `py-10 lg:py-16 xl:py-20 ${crosses ? 'lg:py-32 xl:py-40':''}${className || ''} `}`}>
+      {children}
+      <div className={`absolute top-0 left-5 w-0.25 bg-stroke-1 pointer-events-none h-full md:block lg:left-7.5 xl:left-10 `}/>
+      <div className={`absolute top-0 left-5 w-0.25 bg-stroke-1 pointer-events-none h-full md:block lg:right-7.5 xl:right-10 `}/>
+
+    </div>
   )
 }
 

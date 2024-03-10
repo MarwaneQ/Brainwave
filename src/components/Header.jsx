8ecaf3c311c0a7a14dfa16fa80e0c0ecf6eafd79
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-03-10 04:49:42 -0700

    finishing the hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ import { HambugerMenu } from "./design/Header";
 import { useState } from "react";
 function Header() {
   const pathname = useLocation();
-  const [openNavigation, setOpenNavigation] = useState(true);
+  const [openNavigation, setOpenNavigation] = useState(false);
 
   const toggleNavigation = () => {
     if (openNavigation) {
